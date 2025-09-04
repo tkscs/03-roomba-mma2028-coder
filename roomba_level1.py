1483715a8f788f2bad8ff972f93737201f84c0e2
Matt <mma2028@kehillahstudent.org>
2025-09-04 09:22:17 -0700

    hij

--- roomba_level1.py
+++ roomba_level1.py
@@ -14,7 +14,31 @@ window = room.draw_room(level = 1)
 ###
 # Start your code here
  
- 
+forward(156)
+left(90)
+forward(156)
+left(90)
+forward(156)
+left(90)
+forward(126)
+left(90)
+forward(126)
+left(90)
+forward(95)
+left(90)
+forward(95)
+left(90)
+forward(70)
+left(90)
+forward(70)
+left(90)
+forward(45)
+left(90)
+forward(45)
+left(90)
+forward(20)
+left(90)
+forward(20)
  
 # End your code here
 ###
